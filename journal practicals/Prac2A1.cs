96a06572841ee150cd6387792a70513a1bc313ac
Vedant Khamkar <153050950+vedantkhamkar123@users.noreply.github.com>
2024-09-19 10:26:12 +0530

    Update Prac2A1.cs

--- journal practicals/Prac2A1.cs
+++ journal practicals/Prac2A1.cs
@@ -98,3 +98,37 @@ namespace Prac2A1
         }
     }
 }
+
+#console
+class Factorial
+{
+    public void FactorialMethod()
+    {
+        System.Console.Write("Enter a no := ");
+        int num=int.Parse(System.Console.ReadLine());
+        int a=num,tem=1;
+
+        for(int i=0;i<num;i++)
+        {
+           tem=tem*a;  
+           if(a==1)
+           {
+            System.Console.Write(a);
+           }
+           else
+           {
+            System.Console.Write(a +"*");
+           }
+           a--;
+        }
+        System.Console.WriteLine("\nfactorial of "+num+" is := "+tem);
+    }
+}
+class Facto2A1
+{
+    public static void Main(string[] args)
+    {
+        Factorial fact=new Factorial();
+        fact.FactorialMethod();
+    }
+}
