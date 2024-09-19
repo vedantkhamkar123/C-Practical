3ad5c9bb640d397878e1b8612392ad47d59060a5
Vedant Khamkar <153050950+vedantkhamkar123@users.noreply.github.com>
2024-09-19 11:21:42 +0530

    Update Prac2A3.cs

--- journal practicals/Prac2A3.cs
+++ journal practicals/Prac2A3.cs
@@ -102,3 +102,45 @@ namespace Prac2A3
         }
     }
 }
+
+#console
+class QuadraticDemo
+{
+    public void QuadraticMethod()
+    {
+        int a,b,c,ans;
+        double x1,x2;
+        a=int.Parse(System.Console.ReadLine());
+        b=int.Parse(System.Console.ReadLine());
+        c=int.Parse(System.Console.ReadLine());
+
+        ans=b*b-4*a*c;
+        if(ans==0)
+        {
+            System.Console.WriteLine("both root is real and equal .");
+            x1=-b/2*a;
+            x2=x1;
+            System.Console.WriteLine("Root of x1 is := "+x1+"\nRoot of x2 is := "+x2);
+        }
+        else if(ans>0)
+        {
+            System.Console.WriteLine("Root is real and unequal .");
+            x1=(-b-System.Math.Sqrt(ans))/2*a;
+            x2=(-b+System.Math.Sqrt(ans))/2*a;
+            System.Console.WriteLine("Root of x1 is := "+x1+"\nRoot of x2 is := "+x2);
+        }
+        else
+        {
+            System.Console.WriteLine("Roots are imaginary ..\nNo Solution.");
+        }
+        
+    }
+}
+class Quadratic2A3
+{
+    public static void Main(string[] args)
+    {
+        QuadraticDemo obj=new QuadraticDemo();
+        obj.QuadraticMethod();
+    }
+}
